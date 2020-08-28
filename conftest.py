38e8318e6bbe57fbc5b5af6f202bb450c739e34f
aron-1 <1092587222@qq.com>
2020-08-28 11:15:32 +0800

    驳回发送邮件

--- conftest.py
+++ conftest.py
@@ -53,60 +53,60 @@ def pytest_runtest_makereport(item):
         report.description = str(item.function.__doc__)
 
 
-# @pytest.mark.optionalhook
-# def pytest_html_results_table_header(cells):
-#     cells.insert(1, html.th('用例名称'))
-#     cells.insert(2, html.th('Test_nodeid'))
-#     cells.pop(2)
-#
-#
-# @pytest.mark.optionalhook
-# def pytest_html_results_table_row(report, cells):
-#     cells.insert(1, html.td(report.description))
-#     cells.insert(2, html.td(report.nodeid))
-#     cells.pop(2)
-#
-#
-# @pytest.mark.optionalhook
-# def pytest_html_results_table_html(report, data):
-#     if report.passed:
-#         del data[:]
-#         data.append(html.div('通过的用例未捕获日志输出.', class_='empty log'))
-#
-#
-# @pytest.mark.optionalhook
-# def pytest_html_report_title(report):
-#     report.title = "pytest示例项目测试报告"
-#
-#
-# @pytest.mark.optionalhook
-# def pytest_configure(config):
-#     config._metadata.clear()
-#     config._metadata['测试项目'] = "精准营销"
-#     config._metadata['测试地址'] = ini.url
-#
-#
-# @pytest.mark.optionalhook
-# def pytest_html_results_summary(prefix, summary, postfix):
-#     # prefix.clear() # 清空summary中的内容
-#     prefix.extend([html.p("所属部门: 艾塔公司测试部")])
-#     prefix.extend([html.p("测试执行人: sww")])
-#
-#
-# def pytest_terminal_summary(terminalreporter, exitstatus, config):
-#     """收集测试结果"""
-#     result = {
-#         "total": terminalreporter._numcollected,
-#         'passed': len(terminalreporter.stats.get('passed', [])),
-#         'failed': len(terminalreporter.stats.get('failed', [])),
-#         'error': len(terminalreporter.stats.get('error', [])),
-#         'skipped': len(terminalreporter.stats.get('skipped', [])),
-#         # terminalreporter._sessionstarttime 会话开始时间
-#         'total times': timestamp() - terminalreporter._sessionstarttime
-#     }
-#     print(result)
-#     if result['failed'] or result['error']:
-#         send_report()
+@pytest.mark.optionalhook
+def pytest_html_results_table_header(cells):
+    cells.insert(1, html.th('用例名称'))
+    cells.insert(2, html.th('Test_nodeid'))
+    cells.pop(2)
+
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_row(report, cells):
+    cells.insert(1, html.td(report.description))
+    cells.insert(2, html.td(report.nodeid))
+    cells.pop(2)
+
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_html(report, data):
+    if report.passed:
+        del data[:]
+        data.append(html.div('通过的用例未捕获日志输出.', class_='empty log'))
+
+
+@pytest.mark.optionalhook
+def pytest_html_report_title(report):
+    report.title = "pytest示例项目测试报告"
+
+
+@pytest.mark.optionalhook
+def pytest_configure(config):
+    config._metadata.clear()
+    config._metadata['测试项目'] = "精准营销"
+    config._metadata['测试地址'] = ini.url
+
+
+@pytest.mark.optionalhook
+def pytest_html_results_summary(prefix, summary, postfix):
+    # prefix.clear() # 清空summary中的内容
+    prefix.extend([html.p("所属部门: 艾塔公司测试部")])
+    prefix.extend([html.p("测试执行人: sww")])
+
+
+def pytest_terminal_summary(terminalreporter, exitstatus, config):
+    """收集测试结果"""
+    result = {
+        "total": terminalreporter._numcollected,
+        'passed': len(terminalreporter.stats.get('passed', [])),
+        'failed': len(terminalreporter.stats.get('failed', [])),
+        'error': len(terminalreporter.stats.get('error', [])),
+        'skipped': len(terminalreporter.stats.get('skipped', [])),
+        # terminalreporter._sessionstarttime 会话开始时间
+        'total times': timestamp() - terminalreporter._sessionstarttime
+    }
+    print(result)
+    if result['failed'] or result['error']:
+        send_report()
 
 
 def _capture_screenshot():
