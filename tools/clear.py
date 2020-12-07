4b819a09849c36f624e346363a51991613a047fc
aron-1 <1092587222@qq.com>
2020-12-07 16:42:36 +0800

    脚本修改-中台脚本

--- tools/clear.py
+++ tools/clear.py
@@ -1,8 +1,11 @@
 ﻿import os
 import shutil
-from config.conf import SCREENSHOT_DIR,BASE_DIR
+from config.conf import SCREENSHOT_DIR, BASE_DIR
+from tools.logger import Log
 
 os.chdir(SCREENSHOT_DIR)  # 切换到SCREENSHOT_DIR目录
+
+log = Log().logger
 def picclear():
     # try:
     #     shutil.rmtree('allure-results')  # 清空screen_capture目录下的文件,整个文件夹都删了，不可取
@@ -11,8 +14,11 @@ def picclear():
     for i in os.listdir(SCREENSHOT_DIR):
         if i.endswith(".png"):
             # print(os.listdir(SCREENSHOT_DIR))
-            os.remove(os.path.join(SCREENSHOT_DIR,i))
-            os.mkdir('screen_capture')
+            os.remove(os.path.join(SCREENSHOT_DIR, i))
+            log.info("删除文件夹")
+            if not os.path.exists(SCREENSHOT_DIR):
+                os.makedirs("screen_capture")
+                log.info("重新创建文件夹")
 
 
 os.chdir(BASE_DIR)  # 切换到项目根目录
